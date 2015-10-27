467a35cf58111d32ea712789fe31f383d8ad05bb
Jenny Pletner <jennykay55@gmail.com>
2015-10-26 21:33:33 -0700

    Cleaned up code and revised some content

--- mail2.php
+++ mail2.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $type = $_POST['type'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Email: $email \n Type: $type \n Message: $message";
-$recipient = "info@avritek.com";
+$recipient = "info@avr-recycling.com";
 $subject = "Avritek Site Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!") ;
