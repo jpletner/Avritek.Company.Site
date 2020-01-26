7b02b6a1d2c133b6ffa362f65e66f7e5189c234b
Jenny Pletner <jpletner@gapintelligence.com>
2020-01-25 16:03:29 -0800

    Improves spam checker and adds video clip

--- mail2.php
+++ mail2.php
@@ -1,14 +1,15 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$type = $_POST['type'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Email: $email \n Type: $type \n Message: $message";
-$recipient = "info@avr-recycling.com";
-$subject = "Avritek Site Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!") ;
+  if (request("formChecker") == "" & request("human") == "5") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $type = $_POST['type'];
+    $message = $_POST['message'];
+    $formcontent=" From: $name \n Email: $email \n Type: $type \n Message: $message";
+    $recipient = "info@avr-recycling.com";
+    $subject = "Avritek Site Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!") ;
 
-echo "<div style ='font:28px Merriweather,Arial,sans-serif;text-align:center;font-weight:800;color:rgba(0, 131, 192, 1)'>Thank you! We will be in touch shortly!</div>"
+    echo "<div style ='font:28px Merriweather,Arial,sans-serif;text-align:center;font-weight:800;color:rgba(0, 131, 192, 1)'>Thank you! We will be in touch shortly!</div>"
+  }
 ?>
-
