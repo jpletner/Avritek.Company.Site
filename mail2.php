69005d8bc555faeebacdb3c9680fc8fb78064e77
Jenny Pletner <jpletner@gapintelligence.com>
2020-02-02 16:36:03 -0800

    Updated contact form recipient

--- mail2.php
+++ mail2.php
@@ -5,8 +5,7 @@
       $type = $_POST['serviceType'];
       $message = $_POST['message'];
       $formcontent=" From: $name \n Email: $email \n Type: $type \n Message: $message";
-      // $recipient = "info@avr-recycling.com";
-      $recipient = "jennykay55@gmail.com";
+      $recipient = "info@avr-recycling.com";
       $subject = "Avritek Site Contact Form";
       $mailheader = "From: $email \r\n";
       mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
